e02dfe0f5e04b3989d29424be223012e02608826
sgr13 <s.g.jarzabek@gmail.com>
2018-03-10 22:11:33 +0100

    initial commit

---
+++ src/AppBundle/Controller/Admin/GenusAdminController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace AppBundle\Controller\Admin;
+
+use AppBundle\Form\GenusFormType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * @Route("/admin")
+ */
+class GenusAdminController extends Controller
+{
+    /**
+     * @Route("/genus", name="admin_genus_list")
+     */
+    public function indexAction()
+    {
+        $genuses = $this->getDoctrine()
+            ->getRepository('AppBundle:Genus')
+            ->findAll();
+
+        return $this->render('admin/genus/list.html.twig', array(
+            'genuses' => $genuses
+        ));
+    }
+
+    /**
+     * @Route("/genus/new", name="admin_genus_new")
+     */
+    public function newAction(Request $request)
+    {
+        $form = $this->createForm(GenusFormType::class);
+
+        //only data from post
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $genus = $form->getData();
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($genus);
+            $em->flush();
+
+            $this->addFlash('succes', 'Genus created.');
+
+            return $this->redirectToRoute('admin_genus_list');
+        }
+
+        return $this->render('admin/genus/new.html.twig', array(
+           'genusForm' => $form->createView()
+        ));
+    }
+}
